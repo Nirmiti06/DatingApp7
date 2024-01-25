15bf619417ba2f28f7bba4dca07aeccb3f7fba4d
Nirmiti06 <157284081+Nirmiti06@users.noreply.github.com>
2024-01-25 13:54:08 +0530

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]   //api/users
+[Route("api/[controller]")]   // GET api/users
 public class UsersController : ControllerBase
 {
     private readonly DataContext _context;
