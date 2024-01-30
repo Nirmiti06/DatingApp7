b8f32f0abc57e6fb418036ba4639a6ee49d8e68b
Nirmiti06 <157284081+Nirmiti06@users.noreply.github.com>
2024-01-30 12:14:13 +0530

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,9 +6,9 @@
 
         public string UserName { get; set; }
 
-        // public byte[] PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
 
-        // public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
 
