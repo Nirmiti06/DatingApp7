42f21c4b7c3db7f13e36ce7b3166b69fe386e258
Nirmiti06 <157284081+Nirmiti06@users.noreply.github.com>
2024-01-30 10:22:20 +0530

    End of section 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,21 +4,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Dating app'; 
-  users: any;                        // declaration in javascript
+export class AppComponent implements OnInit{
+  title = 'Dating app';
+  users: any;                 // javascript syntax
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),                  // if there is any error after recieving data from api it will show error
-      complete: () => console.log('Request has completed'),                             
-  }); 
+    this.http.get('http://localhost:5001/api/users').subscribe({
+      next: response => this.users.response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 
-  } 
 }
