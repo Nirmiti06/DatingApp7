a97302b40d214f39f465c318a78d23c7b2b3eda2
Nirmiti06 <157284081+Nirmiti06@users.noreply.github.com>
2024-02-07 22:54:08 +0530

    Enf of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +13,7 @@ import { ToastrService } from 'ngx-toastr';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
+  constructor(public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
   }
